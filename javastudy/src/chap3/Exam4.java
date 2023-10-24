04c7161a84550f22b4feab731fd261f2c0b36f19
unknown <kms@goodee.co.kr>
2023-10-24 15:10:04 +0900

    '20231024-1510'

--- javastudy/src/chap3/Exam4.java
+++ javastudy/src/chap3/Exam4.java
@@ -1,4 +1,7 @@
 package chap3;
+
+import java.util.Scanner;
+
 /*
  * 숫자를 입력받아서 양수인 경우 "양수",음수인 경우 "음수", 0인 경우 "영" 출력하기
  * 1. Scanner 객체 생성
@@ -17,10 +20,11 @@ package chap3;
  * 0 : 영
  */
 public class Exam4 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("숫자를 입력하세요");
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		System.out.println(num + ":" +
+		   ((num > 0)?"양수":(num < 0)?"음수":"영"));
 	}
-
 }
