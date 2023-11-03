1a68148d41a3bd2b911c612a2a00646d663bd904
unknown <kms@goodee.co.kr>
2023-11-03 16:04:14 +0900

    '20231103-1604'

--- javastudy/src/chap7/ModifierEx2.java
+++ javastudy/src/chap7/ModifierEx2.java
@@ -1,13 +1,21 @@
 package chap7;
-
+/*
+ * 접근제어자 : private < (default) < protected < public
+ *   
+ *   멤버변수,멤버메서드 : private < (default) < protected < public 가능
+ *   클래스           : (default) < public 
+ *                    (default) 클래스 : 같은 패키지내에서만 접근 허용
+ *                       public 클래스 :    그외 접근 허용  
+ */
 import chap7.test.Modifier2;
-
+// chap7.Modifier3
+// chap7.test.Modifier2
 class Modifier3 extends Modifier2 {
 	public void method() {
 		System.out.println("chap7.Modifier3 클래스의 method");
-		System.out.println("v1="+v1);
-		System.out.println("v2="+v2);
-		System.out.println("v3="+v3);
+//		System.out.println("v1="+v1); //Modifier2 클래스 내부에서만 접근 허용
+//		System.out.println("v2="+v2); //chap7.test 패키지 내부의 클래스에서만 접근 허용
+		System.out.println("v3="+v3); //Modifier3가 Modifier2의 하위클래스.상속관계정상
 		System.out.println("v4="+v4);
 	}
 }
@@ -19,5 +27,8 @@ public class ModifierEx2 { //chap7.ModifierEx2
 //		System.out.println(m2.v2); //chap7.test 패키지 내부의 클래스 에서만 접근 허용
 //		System.out.println(m2.v3); //상속관계에서만 접근 허용
 		System.out.println(m2.v4); //public 제어자이므로 정상
+		
+		Modifier3 m3 = new Modifier3();
+        m3.method();				
 	}
 }
---
+++ javastudy/src/chap8/InterfaceEx1.java
@@ -0,0 +1,24 @@
+package chap8;
+/*
+ * 인터페이스 
+ * 1. 인터페이스멤버 : 상수,추상메서드, default 메서드, static 메서드만 가능
+ * 2. 인터페이스접근제어자 : public만 가능
+ * 3. 객체화불가 => 구현클래스(인터페이스를 구현)의 객체화로 객체화
+ * 4. 클래스와 인터페이스는 구현으로 표현함. => implements 예약어 사용
+ *    => 다중 구현 가능 
+ * 5. 인터페이스간의 상속 가능 => 다중 상속 가능   
+ * 
+ * 인터페이스 멤버
+ *  상수 :  [public static final] int NUM=100;  (o)
+ *         [public static ] final int NUM=100;  (o)
+ *         protected int NUM=100;              (x)
+ *  추상메서드 :   void method();  (0)        
+ */
+public class InterfaceEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
