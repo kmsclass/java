787dc86b8af4c4b75e377aeed392a39bab53d38e
unknown <kms@goodee.co.kr>
2023-11-08 16:28:04 +0900

    '20231108-1627'

---
+++ javastudy/src/test1108/Test4.java
@@ -0,0 +1,18 @@
+package test1108;
+/*
+ * 소숫점이하 자리수까지 반올림하여 출력하기
+ *    round(double 실수값,int 소숫점이하자리수)  
+ * 소숫점이하 자리수에서 버림하여 출력하기
+ *    truncate(double 실수값,int 소숫점이하자리수)  
+ */
+public class Test4 {
+	public static void main(String[] args) {
+		System.out.println(round(3.1215,1));  //3.1
+		System.out.println(round(3.1215,2));  //3.12
+		System.out.println(round(3.1215,3));  //3.122
+		System.out.println(truncate(3.15345,1));  //3.1
+		System.out.println(truncate(3.15345,2));  //3.15
+		System.out.println(truncate(3.15345,3));  //3.153
+		System.out.println(truncate(3.15345,4));  //3.1534
+	}
+}
