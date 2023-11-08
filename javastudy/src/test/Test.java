e3e0fc2410a9597e1a279045201ee7fe4d6caf1c
unknown <kms@goodee.co.kr>
2023-11-09 08:45:03 +0900

    '20231109-08045'

---
+++ javastudy/src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+class P {
+	int x,y;
+}
+class C extends P{
+	int z;
+}
+public class Test {
+	public static void main(String[] args) {
+		C c = new C();
+	}
+
+}
--- javastudy/src/test1108/Test4.java
+++ javastudy/src/test1108/Test4.java
@@ -4,6 +4,8 @@ package test1108;
  *  double  round(double 실수값,int 소숫점이하자리수)  
  * 소숫점이하 자리수에서 버림하여 출력하기
  *  double  truncate(double 실수값,int 소숫점이하자리수)  
+ *  i=3
+ *  "%3d" => "%"+i+"d" 
  */
 public class Test4 {
 	public static void main(String[] args) {
