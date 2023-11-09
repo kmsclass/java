b419ca2cd4ca89150de427138e3de86658e41476
unknown <kms@goodee.co.kr>
2023-11-10 08:49:38 +0900

    '20231110-0849'

---
+++ javastudy/src/test1109/Test.java
@@ -0,0 +1,10 @@
+package test1109;
+
+public class Test {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("abc");
+		String str = sb.toString();
+	}
+
+}
---
+++ javastudy/src/test1109/Test2_A.java
@@ -0,0 +1,34 @@
+package test1109;
+
+import java.util.Random;
+
+/*
+ * int getRand(f,t) : 함수 구현하기
+ *   f ~ t 또는 t~ f 까지  범위에 숫자를 임의의 수로 리턴하는 함수
+ *   f, t 값은 포함됨. 
+ 
+[결과]
+-2,1,0,-3,-2,1,-1,0,-2,0,0,1,0,-3,0,-1,-1,-2,-2,1,
+3,3,0,0,-1,-1,0,3,2,3,0,-1,1,1,-1,3,0,0,1,-1,    
+ */
+public class Test2_A {
+	public static void main(String[] args) {
+		for(int i=0;i<20;i++) {
+			System.out.print(getRand(1,-3)+",");
+		}
+		System.out.println();
+		for(int i=0;i<20;i++) {
+			System.out.print(getRand(-1,3)+",");
+		}
+	}
+	static int getRand(int f,int t) {
+//		int min = Math.min(f,t);
+		int min = (f > t)?t:f;
+		int max = Math.max(f,t);
+		int cha = max - min;
+//		return (int)(Math.random() * (cha+1)) + min;
+		Random r = new Random();
+		return  r.nextInt(cha+1) + min;
+	}
+	
+}
