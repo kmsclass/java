d643e18d6e0bd8bd1330bb19e9ced7d8a4dfad74
unknown <kms@goodee.co.kr>
2023-11-14 16:06:30 +0900

    '20231114-1606'

--- javastudy/src/test1114/Test2.java
+++ javastudy/src/test1114/Test2.java
@@ -6,6 +6,19 @@ package test1114;
  * 만약 파일이 없는 경우는 '해당 파일 없음' 을 출력하기
  * 한글은 깨지지 않아야 함.
  * FileInputStream 사용하기
+ * 
+ * [결과]
+화면에 파일명을 입력하세요(종료:exit)
+src/chap14/InputStreaEx1.java
+해당 파일 없음
+화면에 파일명을 입력하세요(종료:exit)
+src/chap14/InputStreamEx1.java
+해당 파일 없음
+화면에 파일명을 입력하세요(종료:exit)
+src/test1114/Test1.java
+package test1114;
+....
+.....
  */
 public class Test2 {
 	public static void main(String[] args) {
