e56cc6f1479d4c13a524da550f9c995029b6fb53
unknown <kms@goodee.co.kr>
2023-11-15 10:44:31 +0900

    '20231115-1044'

---
+++ javastudy/src/chap14/Exam3.java
@@ -0,0 +1,12 @@
+package chap14;
+/*
+ * InputStreamEx1.java 소스를 읽어서, InputStreamEx1.bak 파일로 복사하기
+ * FileInputStream,FileOutputStream을 이용하고, 한글 깨지지 않도록 한다
+ */
+public class Exam3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
