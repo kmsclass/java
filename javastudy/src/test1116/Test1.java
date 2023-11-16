1207b086bb370a9c6f843b5f2ecc9fcbd5d25363
unknown <kms@goodee.co.kr>
2023-11-16 15:57:53 +0900

    '20231116-1557'

---
+++ javastudy/src/test1116/Test1.java
@@ -0,0 +1,10 @@
+package test1116;
+/*
+ * ThreadEx8번의 DataAddThread 를 Runnable 인터페이스를 구현한
+ *   방식으로 변경하기 
+ */
+public class Test1 {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ javastudy/src/test1116/Test2.java
@@ -0,0 +1,23 @@
+package test1116;
+
+/*
+ * 영어 타자 연습 프로그램 구현
+ * 
+ * 3초에 한번씩 List에 소문자 3~4자의 임의의 영어단어 추가하기.
+ * 화면에 추가된이름을 출력하기.
+ * 단어를 입력받아서 입력받은 단어를 리스트에서 제거.
+ * List 의 모든 데이터 삭제되면 프로그램 종료
+ * 
+ [결과]
+[abcdef]
+>>abcdef
+[cncm]
+>>cncm
+[rfaa]
+>>rfaa  
+ */
+
+public class Test2 {
+	public static void main(String[] args) {
+	}	
+}
