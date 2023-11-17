666177bdb65f83f090fca23acb30933f0ced4475
unknown <kms@goodee.co.kr>
2023-11-17 15:33:56 +0900

    '20231117-1533'

--- javastudy/src/chap15/Exam1.java
+++ javastudy/src/chap15/Exam1.java
@@ -2,6 +2,9 @@ package chap15;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
 
 /*
  * Function,ToIntFunction을 이용하여 다음 결과가 나오도록 프로그램 작성하기
@@ -25,16 +28,35 @@ class Person {
 	}
 }
 public class Exam1 {
-	static List<Person> list = Arrays.asList(new Person("김삿갓",30),
+	static List<Person> list = Arrays.asList(
+			new Person("홍길동",10),new Person("김삿갓",30),
 	new Person("이몽룡",15),new Person("김자바",25),new Person("박자바",20));
 	public static void main(String[] args) {
 		System.out.print("이름목록:");
-		printString();
+		printString(t->t.name);
 		System.out.print("나이목록:");
-		printInt();
+		printInt(t->t.age);
 		System.out.print("총나이합산:");
-		printTot();
+		printTot(t->t.age);
 		System.out.print("나이 평균:");
-		printAvg();
+		printAvg(t->t.age);
+	}
+	static void printString(Function<Person,String> f) {
+		for(Person p : list) System.out.print(f.apply(p) + ",");
+		System.out.println();
+	}
+	static void printInt(ToIntFunction<Person> f) {
+		for(Person p : list) System.out.print(f.applyAsInt(p)+",");
+		System.out.println();
+	}
+	static void printTot(ToIntFunction<Person> f) {
+		int sum = 0;
+		for(Person p : list) sum += f.applyAsInt(p);
+		System.out.println(sum);
+	}
+	static void printAvg(ToDoubleFunction<Person> f) {
+		double sum = 0;
+		for(Person p : list) sum += f.applyAsDouble(p); //나이데이터 double 리턴
+		System.out.println(sum/list.size());
 	}
 }
